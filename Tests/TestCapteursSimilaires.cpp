1bf3746572d4b9e5320cd9902ddbe04d34b3cd4a
SebGoll <50230837+SebGoll@users.noreply.github.com>
2021-06-03 14:09:23 +0200

    Explication des tests + test 1

--- Tests/TestCapteursSimilaires.cpp
+++ Tests/TestCapteursSimilaires.cpp
@@ -11,8 +11,8 @@ void test2();
 int main(){
 
     test0();
-//    test1();
-    test2();
+    test1();
+//    test2();
 
     return 0;
 }
@@ -20,15 +20,19 @@ int main(){
 void test0(){
     cout<<"==================================\nTest 0 : fonctionnement normal"<<endl;
     cout<<"Resultat attendu:\nCapteur 0 -> 12\nCapteur 1 -> 20\nCapteur 2 -> 0\nCapteur 3 -> 5\nCapteur 4 -> 24"<<endl;
-    genererListeCapteurs("Data/DataSetTest-CapteursSimilaires/sensorsTest.csv");
-    genererListeMesures("Data/DataSetTest-CapteursSimilaires/measurementsTest.csv");
+    genererListeCapteurs("Data/DataSetTest-CapteursSimilaires/sensorsTest0.csv");
+    genererListeMesures("Data/DataSetTest-CapteursSimilaires/measurementsTest0.csv");
+    cout<<"Resultat:"<<endl;
     capteursSimilaires(2,"2019-01-01 12:00:00","2019-01-06 12:00:00");
 
 }
 
 void test1(){
+    cout<<"==================================\nTest 1 : capteurs identiques"<<endl;
+    cout<<"Resultat attendu:\nCapteur 0 -> 0\nCapteur 1 -> 0\nCapteur 2 -> 0\nCapteur 3 -> 0\nCapteur 4 -> 0"<<endl;
     genererListeCapteurs("Data/DataSetTest-CapteursSimilaires/sensorsTest1.csv");
     genererListeMesures("Data/DataSetTest-CapteursSimilaires/measurementsTest1.csv");
+    cout<<"Resultat:"<<endl;
     capteursSimilaires(2,"2019-01-01 12:00:00","2019-01-06 12:00:00");
 }
 void test2(){
